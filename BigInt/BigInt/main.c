7bd9fa4999e25ac9bce8122c3b3f0c62e99ffb33
tranwang1102 <tranwang1102@gmail.com>
2021-07-17 23:52:36 +0700

    create Xcode project

---
+++ BigInt/BigInt/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  BigInt
+//
+//  Created by Trần Quang on 17/07/2021.
+//
+
+#include <stdio.h>
+#include "BigInt.h"
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
